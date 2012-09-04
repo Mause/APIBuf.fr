fad6817396927a71254ebc3117afc448b32b610e
Dominic May <jack.thatch@gmail.com>
2012-09-04 20:23:20 +0800

    Bumped version, moved get_gravatar function to utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
 import re
+import urllib
+import hashlib
+
+
+def get_gravatar(email, size=80, default="http://www.example.com/default.jpg"):
+    # construct the url
+    gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+    gravatar_url += urllib.urlencode({'d': default, 's': str(size)})
+    return gravatar_url
 
 
 def is_valid_url(url):
@@ -11,14 +20,3 @@ def is_valid_url(url):
         r'(?::\d+)?'  # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
     return url is not None and regex.search(url)
-
-
-
-# def is_valid_url(url):
-#     regex = re.compile(
-#         r'^(?:http|ftp)s?://'  # http:// or https://
-#         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-#         r'localhost|'  # localhost...
-#         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
-#         r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
-#         r'(?::\d+)?(?:/?|[/?]\S+)$
